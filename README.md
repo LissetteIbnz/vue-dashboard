ad722e1b3238ac87e590e9ddf3fce8e223dc4539
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-08-17 15:43:52 +0100

    Continue

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ npm test
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+https://github.com/vuejs-templates/webpack/pull/797/files
+https://www.google.es/search?q=ts-loader+Module+build+failed%3A+Error%3A+You+may+be+using+an+old+version+of+webpack%3B+please+check+you%27re+using+at+least+version+4&oq=ts-loader+Module+build+failed%3A+Error%3A+You+may+be+using+an+old+version+of+webpack%3B+please+check+you%27re+using+at+least+version+4&aqs=chrome..69i57.2232j0j1&sourceid=chrome&ie=UTF-8
+
+http://blog.ckgrafico.com/extendiendo-tipo-vue-usar-tus-plugins-typescript/
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,8 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    // app: './src/main.js'
+    app: './src/main.ts' // El punto de entrada de la aplicación debe ser Typescript
   },
   output: {
     path: config.build.assetsRoot,
@@ -32,7 +33,7 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.ts', '.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
@@ -45,6 +46,13 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        },
       },
       {
         test: /\.js$/,
